a1120f4ec8aaa78b0d684a42d2a4b71d697c794e
Amir3345 <151976070+Amir3345@users.noreply.github.com>
2024-02-03 16:55:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,46 +10,54 @@
  * Published URL: [Your Published URL Here]
  *********************************************************************************/
 
-const express = require("express");
-const legoData = require("./modules/legoSets");
+const express = require('express');
+const legoData = require('./Modules/LegoSets.js'); 
 
 const app = express();
-const port = 3000;
+const port = 3000; // You can choose the port you prefer
 
-app.get("/", (req, res) => {
-  res.send("Assignment 2: Your Name - Your Student Id");
-});
 
-app.get("/lego/sets", async (req, res) => {
-  try {
-    await legoData.initialize();
-    const allSets = await legoData.getAllSets();
-    res.json(allSets);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+legoData.initialize().then(() => {
+  console.log('Lego data initialized.');
 
-app.get("/lego/sets/num-demo", async (req, res) => {
-  try {
-    await legoData.initialize();
-    const specificSet = await legoData.getSetByNum("001-1");
-    res.json(specificSet);
-  } catch (error) {
-    res.status(404).send(error);
-  }
-});
+  // Root route
+  app.get('/', (req, res) => {
+    res.send('Assignment 2: Amir hossein Behzad - 144725223');
+  });
 
-app.get("/lego/sets/theme-demo", async (req, res) => {
-  try {
-    await legoData.initialize();
-    const setsByTheme = await legoData.getSetsByTheme("tech");
-    res.json(setsByTheme);
-  } catch (error) {
-    res.status(404).send(error);
-  }
-});
+  // Route to get all Lego sets
+  app.get('/lego/sets', (req, res) => {
+    legoData.getAllSets().then((sets) => {
+      res.json(sets);
+    }).catch((error) => {
+      res.status(500).send(error.message);
+    });
+  });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  // Route to demonstrate getting a Lego set by number
+  app.get('/lego/sets/num-demo', (req, res) => {
+    // You will replace '001-1' with a set number from your data
+    legoData.getSetByNum('001-1').then((set) => {
+      res.json(set);
+    }).catch((error) => {
+      res.status(404).send(error.message);
+    });
+  });
+
+  // Route to demonstrate getting Lego sets by theme
+  app.get('/lego/sets/theme-demo', (req, res) => {
+    // Replace 'tech' with a theme from your data
+    legoData.getSetsByTheme('tech').then((sets) => {
+      res.json(sets);
+    }).catch((error) => {
+      res.status(404).send(error.message);
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+}).catch((error) => {
+  console.error('Failed to initialize lego data:', error);
 });
