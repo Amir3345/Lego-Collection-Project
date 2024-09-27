754491dd59586ac7d20349d687b266f9976e02c6
Amir3345 <151976070+Amir3345@users.noreply.github.com>
2024-09-27 13:06:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('views', __dirname + '/views');
 //app.use(express.static("Public"));
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/Public')); 
 app.set("view engine", "ejs");
 
 require("dotenv").config();
